6a02644f28912bab2de7e69bed1054010fdc085d
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2020-01-20 22:39:26 +0200

    Start implementing actor class.

---
+++ src/actor.cpp
@@ -0,0 +1,51 @@
+#include "actor.h"
+
+#include <glad/glad.h>
+
+namespace core
+{
+    inline constexpr unsigned int DEFAULT_BUFFER_PTR = 0;
+
+    Actor::Actor()
+    {
+        m_vao = DEFAULT_BUFFER_PTR;
+        m_vbo = DEFAULT_BUFFER_PTR;
+        m_triangles = 0;
+    }
+
+    Actor::Actor(float vertices[], unsigned int verticesSize, unsigned int triangles)
+    {
+        m_vao = 0;
+        m_vbo = 0;
+        m_triangles = triangles;
+
+        glGenVertexArrays(1, &m_vao);
+        glGenBuffers(1, &m_vbo);
+
+        glBindVertexArray(m_vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+        glBufferData(GL_ARRAY_BUFFER, verticesSize, vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
+        glEnableVertexAttribArray(0);
+
+        glBindVertexArray(DEFAULT_BUFFER_PTR);
+        glBindBuffer(GL_ARRAY_BUFFER, DEFAULT_BUFFER_PTR);
+    }
+
+    void Actor::draw()
+    {
+        glBindVertexArray(m_vao);
+        glDrawArrays(GL_TRIANGLES, 0, m_triangles);
+        glBindVertexArray(DEFAULT_BUFFER_PTR);
+    }
+
+    void Actor::dispose()
+    {
+        glDeleteVertexArrays(1, &m_vao);
+        glDeleteBuffers(1, &m_vbo);
+
+        m_vao = m_vbo = DEFAULT_BUFFER_PTR;
+    }
+}
---
+++ src/actor.h
@@ -0,0 +1,22 @@
+#ifndef ACTOR
+#define ACTOR
+
+namespace core
+{
+    class Actor
+    {
+    private:
+        unsigned int m_vao;
+        unsigned int m_vbo;
+        unsigned int m_triangles;
+
+    public:
+        Actor();
+        Actor(float vertices[], unsigned int verticesSize, unsigned int triangles);
+
+        void draw();
+        void dispose();
+    };
+}
+
+#endif
