6955978a95d554ecaadeb293aa8cfee628af8929
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2020-01-26 14:22:02 +0200

    Implement more complicated demo.

--- src/demo.cpp
+++ src/demo.cpp
@@ -13,6 +13,8 @@
 core::Actor     actor{};
 core::Mesh      mesh{};
 core::Shader*   shader{};
+core::Actor     actor2{};
+core::Shader* shader2{};
 
 void init()
 {
@@ -28,27 +30,41 @@ void init()
     };
     shader = new core::Shader("src/shaders/vertex.vert", "src/shaders/fragment.frag");
     shader->setColor(glm::vec4{ 0.98, 0.55, 0.0, 1.0 });
+
+    shader2 = new core::Shader("src/shaders/vertex.vert", "src/shaders/fragment.frag");
+    shader2->setColor(glm::vec4{ 0.65, 0.84, 0.65, 1.0 });
+
     mesh.setVertices(vertices, sizeof(vertices), sizeof(vertices) / sizeof(vertices[0]) / 3);
+
     actor.setMesh(&mesh);
     actor.setShader(shader);
-    actor.setPosition(glm::vec3{ 0.5f, -0.5f, 0.0f });    
+    actor.setPosition(glm::vec3{ 0.5f, -0.5f, 0.0f });
+
+    actor2.setMesh(&mesh);
+    actor2.setShader(shader2);
+    actor2.setPosition(glm::vec3{ -0.5f, 0.5f, 0.0f });
 }
 
 void tick()
 {
     std::cout << "-------- TICK: " << utils::GameTime::getDeltaTime() << " --------" << '\n';
+
     glm::vec3 actorRotation = actor.getRotation();
     actorRotation.z += utils::GameTime::getDeltaTime() * 50.0f;
     actor.setRotation(actorRotation);
-    float scale = static_cast<float>(sin(utils::GameTime::getCurrentTime() * 1.5f) + 1.0f) / 2.0f;
-    actor.setScale(glm::vec3{ scale, scale, 0.5f });
     actor.draw();
+
+    float scale = static_cast<float>(sin(utils::GameTime::getCurrentTime() * 1.5f) + 1.0f) / 2.0f;
+    actor2.setScale(glm::vec3{ scale, scale, 0.5f });
+    actor2.draw();
+    
 }
 
 void dispose()
 {
     std::cout << "-------- DISPOSE --------" << '\n';
     delete shader;
+    delete shader2;
 };
 
 void processKeyboardInput(GLFWwindow* window)
