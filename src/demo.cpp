7f55a4e435b75c033e3250f6a32b6fea28ef31cd
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2020-02-02 20:50:20 +0200

    Remove test code.

--- src/demo.cpp
+++ src/demo.cpp
@@ -83,18 +83,4 @@ void processKeyboardInput(GLFWwindow* window)
          0.5f, -0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f
     };
-
-    unsigned int VBO;
-    unsigned int VAO;
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
-    glEnableVertexAttribArray(0);
 };
